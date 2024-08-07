a0fff3e47939f1d6e30c2f5d0daa74a8ec39f451
Hossam Hatem <hosam.hatem222@gmail.com>
2024-08-07 19:44:06 +0300

    update files

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,6 +1,47 @@
+'use strict';
+
+import { updateWeather, error404 } from "./app.js"
+
+
+const defauttLocation = "#/weather?lat=30.0443879&lon=31.2357257" // Cairo
+
+
+const currentLocation = () => {
+    window.navigator.geolocation.getCurrentPosition(res => {
+        const {latitude, longitude } = res.coords;
+        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    }, err => {
+        window.location.hash = defauttLocation;
+    });
+}
+
 /**
- * @license MIT
- * @fileoverview Menage all routes
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @param {string} query Searched query 
  */
+const searchedLocation = (query)=> updateWeather(...query.split("&"));
+// updateWeather("lat=51.5073219", "lon=-0.1276474")
+
+
+const routes = new Map([
+    ["/current-location", currentLocation],
+    ["/weather", searchedLocation],
+]);
+
+const checkHash = ()=>{
+    const requestURL = window.location.hash.slice(1);
+    const [route, query] = requestURL.includes? requestURL.split("?"): [requestURL];
+    routes.get(route)? routes.get(route)(query) : error404();
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", ()=>{
+    if(!window.location.hash){
+        window.location.hash = "#/current-location";
+    }
+    else {
+        checkHash();
+    }
+});
+
+
